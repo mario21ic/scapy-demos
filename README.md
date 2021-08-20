50f02bcd8820417a808332e80df4fcc81016a8cf
Mario Inga <mario21ic@gmail.com>
2021-08-20 18:06:16 -0500

    feat(global) improving icmp

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Running a script:
 ```
 $ python dns.py mario21ic.com
 ```
+
+Sending an icmp with customized source (false mac addres, false ip and dest ip):
+```
+$ python icmp4_false_src.py 0:e0:4c:f1:ce:a2 192.168.1.10  192.168.1.10
+```
--- icmp4_false_src.py
+++ icmp4_false_src.py
@@ -3,8 +3,15 @@
 import sys
 from scapy.all import *
 
+src_mac = sys.argv[1]
+src_ip = sys.argv[2]
+dst_ip = sys.argv[3]
+
 #ip = IP(src="127.1.2.3", dst="192.168.1.10", ttl=128, tos=9)
-ip = IP(src=sys.argv[1], dst="192.168.1.10", ttl=128, tos=9)
+#ip = IP(src=sys.argv[1], dst="192.168.1.10", ttl=128, tos=9)
+#ip = Ether()/IP(src=sys.argv[1], dst="192.168.1.10", ttl=128, tos=9)
+#ip = Ether(src="0:e0:4c:f1:ce:a2")/IP(src=sys.argv[1], dst="192.168.1.10", ttl=128, tos=9)
+ip = Ether(src=src_mac)/IP(src=src_ip, dst=dst_ip, ttl=128, tos=9)
 
 # Prefijos
 # “X (Ej: XByteField): Hace referencia a que se visualizará y tendrá formato hexadecimal: 0x05.
@@ -14,7 +21,7 @@ ip = IP(src=sys.argv[1], dst="192.168.1.10", ttl=128, tos=9)
 # ip.tos = 0xA
 
 # sin procesar
-print(ip.show())
+#print(ip.show())
 
 # simula procesado
 print(ip.show2())
@@ -27,5 +34,9 @@ ICMP: https://tools.ietf.org/rfc/rfc792.txt
 ARP: https://tools.ietf.org/rfc/rfc826.txt”
 """
 
-print(send(ip/ICMP()))
+# send solo para IP
+#print(send(ip/ICMP()/"hello xD"))
+
+# cuando se necesita Ether
+print(sendp(ip/ICMP()/"hello from scapy"))
 
