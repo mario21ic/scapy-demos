9cab66208c07dab769c21cb2bddcec9cf4cf2998
Mario Inga <mario21ic@gmail.com>
2021-08-23 01:57:24 -0500

    feat(global) adding send with sr1 examples

--- send_sr.py
+++ send_sr.py
@@ -2,6 +2,11 @@
 
 from scapy.all import *
 
+"""
+Note:
+srbt only for bluetooth
+srloop for n packages and times, for layer 2 srploop
+"""
 
 print("## Capa 3")
 pkg3 = IP(dst="google.com")/ICMP(seq=[x for x in range(2)])
@@ -11,6 +16,8 @@ ans3, unans3 = sr(pkg3)
 #ans3 = sr1(pkg2) # only first answer
 for x in ans3:
     print(x)
+print(TCP in ans3)
+print(ICMP in ans3)
 
 print("## Capa 2")
 #pkg2 = Ether()/IP(dst="google.com")/ICMP()
@@ -21,9 +28,6 @@ ans2, unans2 = srp(pkg2)
 #ans2 = srp1(pkg2) # only first answer
 for x in ans2:
     print(x)
+print(TCP in ans3)
+print(ICMP in ans3)
 
-"""
-Note:
-srbt only for bluetooth
-srloop for n packages and times, for layer 2 srploop
-"""
---
+++ send_sr1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from scapy.all import *
+
+print("## Capa 2 - srp1")
+#pkg1 = Ether()/IP(dst="google.com")/TCP(dport=80, flags="S")
+pkg1 = Ether()/IP(dst="192.168.1.10")/TCP(dport=80, flags="S")
+print(pkg1.show())
+print(pkg1.summary())
+ans1 = srp1(pkg1, verbose=0)
+print(TCP in ans1)
+print(ICMP in ans1)
+print(ans1)
+# Nota: desde local se manda un SYN y el target debe intentar devolver un SYNC, ACK
+
+input("Presione cualquier tecla para continuar el reenvio...")
+
+print("## Capa 2 - srp1 - sendp")
+my_tcp = ans1[TCP] # Extraemos su layer TCP
+print(my_tcp.show())
+my_tcp.flasgs = "S"
+#pkg1_1 = Ether()/IP(dst="google.com")/my_tcp
+pkg1_1 = Ether()/IP(dst="192.168.1.10")/my_tcp
+r2 = sendp(pkg1_1, iface="en0", verbose=0)
+print(pkg1_1.show())
+print(pkg1_1.summary())
+print(r2)
